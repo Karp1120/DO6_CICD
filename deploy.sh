82edbab775e201e4506eb090e52f1603955beb07
nissafre <antonov.05068@gmail.com>
2025-09-23 21:41:13 +0300

    8

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
-ARTIFACT=data-samples/DO
-TARGET_DIR="/usr/local/bin"
-VM2_IP="192.168.56.102"
-DEPLOY_USER="deploy"
-scp -i /home/gitlab-runner/.ssh/id_ed25519 $ARTIFACT ${DEPLOY_USER}@${VM2_IP}:${TARGET_DIR}/
 
+scp -i /home/gitlab-runner/.ssh/id_ed25519 data-samples/DO deploy@192.168.56.102:/tmp/DO
 
-ssh -i /home/gitlab-runner/.ssh/id_ed25519 ${DEPLOY_USER}@${VM2_IP} "${TARGET_DIR}/${ARTIFACT} 1"
-
+ssh -i /home/gitlab-runner/.ssh/id_ed25519 deploy@192.168.56.102 "sudo install -m 755 /tmp/DO /usr/local/bin/DO && rm -f /tmp/DO && DO 1"
 
